1a55a5602d7729cb774832df9037f91b9a49c189
ogi <marko.ognjenovich@gmail.com>
2020-09-17 12:05:14 +0200

    Updates

---
+++ precache-manifest.11d8254daa0c771a3fb5a01b09a03d37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a65557a0ae8556570dce47f0a1c55f",
+    "url": "/stock-monitoring-app1/index.html"
+  },
+  {
+    "revision": "a9cb3e66d83c00d83e11",
+    "url": "/stock-monitoring-app1/static/css/2.809f38ef.chunk.css"
+  },
+  {
+    "revision": "aa521701344d9a5fe6cf",
+    "url": "/stock-monitoring-app1/static/css/main.82c32bb2.chunk.css"
+  },
+  {
+    "revision": "a9cb3e66d83c00d83e11",
+    "url": "/stock-monitoring-app1/static/js/2.7c262d93.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/stock-monitoring-app1/static/js/2.7c262d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa521701344d9a5fe6cf",
+    "url": "/stock-monitoring-app1/static/js/main.bacb977e.chunk.js"
+  },
+  {
+    "revision": "a467e4bb70b2e086ccbc",
+    "url": "/stock-monitoring-app1/static/js/runtime-main.236bb4b5.js"
+  },
+  {
+    "revision": "c686b98aae03971428ed7a75a763baaf",
+    "url": "/stock-monitoring-app1/static/media/bank.c686b98a.png"
+  }
+]);
